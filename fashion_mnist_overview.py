94a31d8e4daf47cd68fa6fe350dfb42896032953
dassrikrishna <srikrishnadas319@gmail.com>
2025-02-27 20:40:50 +0530

    Update fashion_mnist_overview.py

--- fashion_mnist_overview.py
+++ fashion_mnist_overview.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 from keras.datasets import fashion_mnist
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-print(x_train[0])
+plt.imshow(x_train[0])
+plt.show()
