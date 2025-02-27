347ead0239fdf4a2dde80c53c8f4ae8888da0f8d
dassrikrishna <srikrishnadas319@gmail.com>
2025-02-27 21:09:34 +0530

    Update fashion_mnist_overview.py

--- fashion_mnist_overview.py
+++ fashion_mnist_overview.py
@@ -4,6 +4,7 @@ from keras.datasets import fashion_mnist
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
-index = np.where(y_train == 0)[0][0]
-plt.imshow(x_train[index])
-plt.show()
+for i in range(10):
+    index = np.where(y_train == i)[0][0]
+    plt.imshow(x_train[index])
+    plt.show()
