2c0a802eac7d02578b53a809d7734e3558f42c8b
dassrikrishna <srikrishnadas319@gmail.com>
2025-02-27 22:09:10 +0530

    Update fashion_mnist_overview.py

--- fashion_mnist_overview.py
+++ fashion_mnist_overview.py
@@ -5,10 +5,14 @@ from keras.datasets import fashion_mnist
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 fig, axes = plt.subplots(2,5,figsize=(10, 5))
+fig.suptitle("Fashion-MNIST Images")
 axes = axes.ravel()
+
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
 for i in range(10):
     index = np.where(y_train == i)[0][0]
+    axes[i].set_title(class_names[i])
     axes[i].imshow(x_train[index])
 
 plt.show()  
