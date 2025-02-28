c7b6cb3751f70aa2846df194abc81a2eed40931b
dassrikrishna <srikrishnadas319@gmail.com>
2025-02-28 13:00:33 +0530

    initialize weights "random normal" or "Xavier"

--- feedforward.py
+++ feedforward.py
@@ -11,7 +11,21 @@ class Feedforward:
           #intialize weights and biases
           self.weights, self.biases = self.initialize_weights(input_size, hidden_size, output_size, num_layers, weight_init)
      
-     
+     #initialize weights "random normal" or "Xavier"
      def initialize_weights(self, input_size, hidden_size, output_size, num_layers, weight_init):
         weights = []
         biases = []
+
+        layer_sizes = [input_size] + [hidden_size] * num_layers + [output_size]
+        for i in range(len(layer_sizes) - 1):
+            if weight_init == "random":
+                weight_matrix = np.random.normal(0, 1, (layer_sizes[i], layer_sizes[i + 1]))
+            elif weight_init == "Xavier":
+                sigma = np.sqrt(6 / (layer_sizes[i] + layer_sizes[i + 1]))
+                weight_matrix = np.random.uniform(-sigma, sigma, (layer_sizes[i], layer_sizes[i + 1]))
+            
+            bias_vector = np.zeros((1, layer_sizes[i + 1]))
+            weights.append(weight_matrix)
+            biases.append(bias_vector)
+        
+        return weights, biases
