7c0dd7e8e474c762e7263545c7e4dbda673ffe4d
dassrikrishna <srikrishnadas319@gmail.com>
2025-03-03 01:12:33 +0530

    Update reshape_data.py

--- reshape_data.py
+++ reshape_data.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-def reshape(x):
+def transform(x):
     # normalize pixel values to [0,1] range
     x = x / 255.0
     # reshape the data
     x = x.reshape(-1, 784)
+    return x
 
 def one_hot(labels, num_classes=10):
     one_hot = np.zeros((labels.size, num_classes))
