68ddfa0d81222306171e68e49e8e16e22658037e
dassrikrishna <srikrishnadas319@gmail.com>
2025-03-05 23:44:47 +0530

    Update backpropagation.py

--- backpropagation.py
+++ backpropagation.py
@@ -49,13 +49,13 @@ def minibatch_gd(X, Y, weights, biases, num_layers, activation, learning_rate, e
 
   return weights, biases
 
-# momentum based gradient descent
+# minibatch momentum based gradient descent
 def minibatch_mgd(X, Y, weights, biases, num_layers, activation, learning_rate, epochs, batch_size, momentum):
   samples_size = X.shape[0]
   prev_uw = [np.zeros_like(w) for w in weights]
   prev_ub = [np.zeros_like(b) for b in biases]
 
-  for epoch in range(epochs):
+  for _ in range(epochs):
     for i in range(0, samples_size, batch_size):
         X_batch = X[i:i + batch_size]
         Y_batch = Y[i:i + batch_size]
@@ -77,4 +77,146 @@ def minibatch_mgd(X, Y, weights, biases, num_layers, activation, learning_rate, 
 
   return weights, biases
 
-# 
+# minibatch nesterov accelerated gradient descent
+def minibatch_nag(X, Y, weights, biases, num_layers, activation, learning_rate, epochs, batch_size, momentum):
+  samples_size = X.shape[0]
+  prev_vw = [np.zeros_like(w) for w in weights]
+  prev_vb = [np.zeros_like(b) for b in biases]
+
+  for _ in range(epochs):
+    for i in range(0, samples_size, batch_size):
+        X_batch = X[i:i + batch_size]
+        Y_batch = Y[i:i + batch_size]
+
+        dw = [np.zeros_like(w) for w in weights]
+        db = [np.zeros_like(b) for b in biases]
+
+        # Lookahead
+        v_w = [momentum * pvw for pvw in prev_vw]
+        v_b = [momentum * pvb for pvb in prev_vb]
+
+        for j in range(len(weights)):
+          weights[j] -= v_w[j]
+          biases[j] -= v_b[j]
+        
+        for x, y in zip(X_batch, Y_batch):
+          grads_W, grads_b = compute_grads(x, y, num_layers, weights, biases, activation)
+          for j in range(len(weights)):
+            dw[j] += grads_W[j]
+            db[j] += grads_b[j]
+
+        for j in range(len(weights)):
+          prev_vw[j] = momentum * prev_vw[j] + (learning_rate / len(X_batch)) * dw[j]
+          prev_vb[j] = momentum * prev_vb[j] + (learning_rate / len(X_batch)) * db[j]
+          weights[j] -= prev_vw[j]
+          biases[j] -= prev_vb[j]
+
+  return weights, biases
+
+# minibatch rmsprop
+def minibatch_rmsprop(X, Y, weights, biases, num_layers, activation, learning_rate, epochs, batch_size, beta, epsilon):
+  samples_size = X.shape[0]
+  v_w = [np.zeros_like(w) for w in weights]
+  v_b = [np.zeros_like(b) for b in biases]
+
+  for _ in range(epochs):
+    for i in range(0, samples_size, batch_size):
+        X_batch = X[i:i + batch_size]
+        Y_batch = Y[i:i + batch_size]
+
+        dw = [np.zeros_like(w) for w in weights]
+        db = [np.zeros_like(b) for b in biases]
+        
+        for x, y in zip(X_batch, Y_batch):
+          grads_W, grads_b = compute_grads(x, y, num_layers, weights, biases, activation)
+          for j in range(len(weights)):
+            dw[j] += grads_W[j]
+            db[j] += grads_b[j]
+        
+        for j in range(len(weights)):
+            v_w[j] = beta * v_w[j] + (1 - beta) * (dw[j] / len(X_batch))**2
+            v_b[j] = beta * v_b[j] + (1 - beta) * (db[j] / len(X_batch))**2
+
+            weights[j] -= (learning_rate * (dw[j] / len(X_batch))) / (np.sqrt(v_w[j]) + epsilon)
+            biases[j] -= (learning_rate * (db[j] / len(X_batch))) / (np.sqrt(v_b[j]) + epsilon)
+
+  return weights, biases
+
+# minibatch adam gradient decent
+def minibatch_adam(X, Y, weights, biases, num_layers, activation, learning_rate, epochs, batch_size, beta1, beta2, epsilon):
+  samples_size = X.shape[0]
+  m_w = [np.zeros_like(w) for w in weights]
+  v_w = [np.zeros_like(w) for w in weights]
+  m_b = [np.zeros_like(b) for b in biases]
+  v_b = [np.zeros_like(b) for b in biases]
+
+  for epoch in range(epochs):
+    for i in range(0, samples_size, batch_size):
+        X_batch = X[i:i + batch_size]
+        Y_batch = Y[i:i + batch_size]
+
+        dw = [np.zeros_like(w) for w in weights]
+        db = [np.zeros_like(b) for b in biases]
+        
+        for x, y in zip(X_batch, Y_batch):
+          grads_W, grads_b = compute_grads(x, y, num_layers, weights, biases, activation)
+          for j in range(len(weights)):
+            dw[j] += grads_W[j]
+            db[j] += grads_b[j]
+        
+        for j in range(len(weights)):
+            m_w[j] = beta1 * m_w[j] + (1 - beta1) * (dw[j] / len(X_batch))
+            v_w[j] = beta2 * v_w[j] + (1 - beta2) * ((dw[j] / len(X_batch))**2)
+            m_b[j] = beta1 * m_b[j] + (1 - beta1) * (db[j] / len(X_batch))
+            v_b[j] = beta2 * v_b[j] + (1 - beta2) * ((db[j] / len(X_batch))**2)
+
+            m_w_cap = m_w[j] / (1 - beta1**(epoch + 1))
+            v_w_cap = v_w[j] / (1 - beta2**(epoch + 1))
+            m_b_cap = m_b[j] / (1 - beta1**(epoch + 1))
+            v_b_cap = v_b[j] / (1 - beta2**(epoch + 1))
+
+            weights[j] -= (learning_rate * m_w_cap) / (np.sqrt(v_w_cap) + epsilon)
+            biases[j] -= (learning_rate * m_b_cap) / (np.sqrt(v_b_cap) + epsilon)
+
+  return weights, biases
+
+# minibatch nadam  gradient decent
+def minibatch_nadam(X, Y, weights, biases, num_layers, activation, learning_rate, epochs, batch_size, beta1, beta2, epsilon):
+    samples_size = X.shape[0]
+    m_w = [np.zeros_like(w) for w in weights]
+    v_w = [np.zeros_like(w) for w in weights]
+    m_b = [np.zeros_like(b) for b in biases]
+    v_b = [np.zeros_like(b) for b in biases]
+
+    for epoch in range(epochs):
+        for i in range(0, samples_size, batch_size):
+            X_batch = X[i:i + batch_size]
+            Y_batch = Y[i:i + batch_size]
+
+            dw = [np.zeros_like(w) for w in weights]
+            db = [np.zeros_like(b) for b in biases]
+
+            for x, y in zip(X_batch, Y_batch):
+                grads_W, grads_b = compute_grads(x, y, num_layers, weights, biases, activation)
+                for j in range(len(weights)):
+                    dw[j] += grads_W[j]
+                    db[j] += grads_b[j]
+
+            for j in range(len(weights)):
+                m_w[j] = beta1 * m_w[j] + (1 - beta1) * (dw[j] / len(X_batch))
+                v_w[j] = beta2 * v_w[j] + (1 - beta2) * ((dw[j] / len(X_batch))**2)
+                m_b[j] = beta1 * m_b[j] + (1 - beta1) * (db[j] / len(X_batch))
+                v_b[j] = beta2 * v_b[j] + (1 - beta2) * ((db[j] / len(X_batch))**2)
+
+                m_w_cap = m_w[j] / (1 - beta1**(epoch + 1))
+                v_w_cap = v_w[j] / (1 - beta2**(epoch + 1))
+                m_b_cap = m_b[j] / (1 - beta1**(epoch + 1))
+                v_b_cap = v_b[j] / (1 - beta2**(epoch + 1))
+
+                # nesterov momentum
+                m_w_lookahead = beta1 * m_w_cap + (1 - beta1) * (dw[j] / len(X_batch)) / (1 - beta1**(epoch + 1))
+
+                weights[j] -= (learning_rate * m_w_lookahead) / (np.sqrt(v_w_cap) + epsilon)
+                biases[j] -= (learning_rate * m_b_cap) / (np.sqrt(v_b_cap) + epsilon)
+
+    return weights, biases
