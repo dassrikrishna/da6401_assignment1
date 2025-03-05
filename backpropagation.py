b98af2b52bb6773cc216e8dd3d15a77edb468364
dassrikrishna <srikrishnadas319@gmail.com>
2025-03-05 19:48:41 +0530

    Update backpropagation.py

--- backpropagation.py
+++ backpropagation.py
@@ -1,17 +1,22 @@
 import numpy as np
+from activation import activation_functions
+from feedforward import forward
 
-def compute_grads(model, x, y):
-    z, a = model.forward(x)
-    y_cap = a[-1]
-    error = y_cap - y        # del(L)/del(y_cap)
-    grads_W, grads_b = [], []
+# calculate gradient of weights and biases
+def compute_grads(x, y, num_layers, weights, biases, activation):
+  z, a = forward(x, num_layers, weights, biases, activation)
+  y_cap = a[-1]
+  error = y_cap - y        # del(L)/del(y_cap)
+  grads_W, grads_b = [], []
 
-    for k in reversed(range(len(model.weights))):
-        delta = error * model.activation_derivative(z[k])   # del(L)/del(z)
-        grads_W.insert(0, np.outer(a[k], delta))
-        grads_b.insert(0, delta)
-        
-        if k > 0:
-            error = np.dot(delta, model.weights[k].T)      # del(L)/del(a)
-    
-    return grads_W, grads_b
+  activation_derivative = activation_functions[activation][1]
+
+  for k in reversed(range(len(weights))):
+      delta = error * activation_derivative(z[k])   # del(L)/del(z)
+      grads_W.insert(0, np.outer(a[k], delta))
+      grads_b.insert(0, delta)
+
+      if k > 0:
+          error = np.dot(delta, weights[k].T)      # del(L)/del(a)
+
+  return grads_W, grads_b
