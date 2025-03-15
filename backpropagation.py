dd9af25164bf0c287d53b2b902484e9c493e7a21
dassrikrishna <srikrishnadas319@gmail.com>
2025-03-15 11:19:29 +0530

    Update backpropagation.py

--- backpropagation.py
+++ backpropagation.py
@@ -4,20 +4,20 @@ from feedforward import forward
 
 # calculate gradient of weights and biases (backpropagation)
 def compute_grads(x, y, num_layers, weights, biases, activation):
-  z, a = forward(x, num_layers, weights, biases, activation)
+  z, a = forward(x, num_layers, weights, biases, activation) # z---preactivation, a----activation
   y_cap = a[-1]
-  error = y_cap - y        # del(L)/del(y_cap) ------ w.r.to cross entropy loss function
+  delta = y_cap - y        # del(L)/del(z)
   grads_W, grads_b = [], []
 
   activation_derivative = activation_functions[activation][1]
 
   for k in reversed(range(len(weights))):
-      delta = error * activation_derivative(z[k])   # del(L)/del(z)
       grads_W.insert(0, np.outer(a[k], delta))
       grads_b.insert(0, delta)
 
+      error = np.dot(delta, weights[k].T) # del(L)/del(a)
       if k > 0:
-          error = np.dot(delta, weights[k].T)      # del(L)/del(a)
+        delta = activation_derivative(z[k-1]) * error   # del(L)/del(z)
 
   return grads_W, grads_b
 
