70427f82ec6565d20474b07a319c9b7026c2ec3e
dassrikrishna <srikrishnadas319@gmail.com>
2025-03-16 19:13:06 +0530

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -3,11 +3,7 @@ import numpy as np
 from backpropagation import *
 from feedforward import forward
 from loss_fun import *
-######---------------
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-import matplotlib.pyplot as plt
-##############------------
+from taccuracy_confusion import compute_accuracy
 
 # optimizer:
 ###################################################
@@ -69,22 +65,7 @@ def minibatch_gd(X, Y, weights, biases, num_layers, learning_rate, epochs, batch
         })
       
         print("epoch:", epoch + 1, "loss:", epoch_loss," & accuracy:", accuracy)
-#########---------
-    # compute confusion matrix
-    Y_pred_labels = np.argmax(Y_val_cap, axis = 1)
-    Y_true_labels = np.argmax(Y_val, axis = 1)
-    conf_matrix = confusion_matrix(Y_true_labels, Y_pred_labels)
-
-    # plot confusion matrix
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(conf_matrix, annot = True, fmt = "d", cmap = "Blues")
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
-    plt.title("Confusion Matrix")
-
-    #  log confusion matrix to wandb
-    wandb.log({"confusion_matrix": wandb.Image(plt)})
-    #return losses
+    return weights, biases
 
 ##################################
 # minibatch momentum based gradient descent
@@ -149,23 +130,7 @@ def minibatch_mgd(X, Y, weights, biases, num_layers, activation, learning_rate, 
         })
       
         print("epoch:", epoch + 1, "loss:", epoch_loss," & accuracy:", accuracy)
-#########---------
-    # compute confusion matrix
-    Y_pred_labels = np.argmax(Y_val_cap, axis = 1)
-    Y_true_labels = np.argmax(Y_val, axis = 1)
-    conf_matrix = confusion_matrix(Y_true_labels, Y_pred_labels)
-
-    # plot confusion matrix
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(conf_matrix, annot = True, fmt = "d", cmap = "Blues")
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
-    plt.title("Confusion Matrix")
-
-    #  log confusion matrix to wandb
-    wandb.log({"confusion_matrix": wandb.Image(plt)})
-
-    #return losses
+    return weights, biases
 
 #####################################
 # minibatch nesterov accelerated gradient descent
@@ -236,23 +201,7 @@ def minibatch_nag(X, Y, weights, biases, num_layers, activation, learning_rate, 
         })
       
         print("epoch:", epoch + 1, "loss:", epoch_loss," & accuracy:", accuracy)
-#########---------
-    # compute confusion matrix
-    Y_pred_labels = np.argmax(Y_val_cap, axis = 1)
-    Y_true_labels = np.argmax(Y_val, axis = 1)
-    conf_matrix = confusion_matrix(Y_true_labels, Y_pred_labels)
-
-    # plot confusion matrix
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(conf_matrix, annot = True, fmt = "d", cmap = "Blues")
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
-    plt.title("Confusion Matrix")
-
-    #  log confusion matrix to wandb
-    wandb.log({"confusion_matrix": wandb.Image(plt)})
-##############------
-    #return losses
+    return weights, biases
 
 ##########################################################
 def minibatch_rmsprop(X, Y, weights, biases, num_layers, activation, learning_rate, epochs, batch_size, beta, epsilon, loss_fun, weight_decay):
@@ -316,29 +265,14 @@ def minibatch_rmsprop(X, Y, weights, biases, num_layers, activation, learning_ra
         })
       
         print("epoch:", epoch + 1, "loss:", epoch_loss," & accuracy:", accuracy)
-#########---------
-    # compute confusion matrix
-    Y_pred_labels = np.argmax(Y_val_cap, axis = 1)
-    Y_true_labels = np.argmax(Y_val, axis = 1)
-    conf_matrix = confusion_matrix(Y_true_labels, Y_pred_labels)
-
-    # plot confusion matrix
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(conf_matrix, annot = True, fmt = "d", cmap = "Blues")
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
-    plt.title("Confusion Matrix")
-
-    #  log confusion matrix to wandb
-    wandb.log({"confusion_matrix": wandb.Image(plt)})
-######-------------------
-    #return losses
+    return weights, biases
 
 ####################
+
 # minibatch adam gradient decent
 def minibatch_adam(X, Y, weights, biases, num_layers, activation, learning_rate, epochs, batch_size, beta1, beta2, epsilon, loss_fun, weight_decay):
     samples_size = X.shape[0]
-
+    t = 0 # step
     # shuffle dataset before splitting
     indices = np.random.permutation(samples_size)
     X, Y = X[indices], Y[indices]
@@ -367,20 +301,20 @@ def minibatch_adam(X, Y, weights, biases, num_layers, activation, learning_rate,
 
             for x, y in zip(X_batch, Y_batch):
                 grads_W, grads_b = compute_grads(x, y, num_layers, weights, biases, activation, weight_decay)
-                for j in range(len(weights)):
-                    dw[j] = grads_W[j]
-                    db[j] = grads_b[j]
-
+                
+                dw = grads_W
+                db = grads_b
+            t +=1 # increment step
             for j in range(len(weights)):
                 m_w[j] = beta1 * m_w[j] + (1 - beta1) * (dw[j] / len(X_batch))
                 v_w[j] = beta2 * v_w[j] + (1 - beta2) * ((dw[j] / len(X_batch))**2)
                 m_b[j] = beta1 * m_b[j] + (1 - beta1) * (db[j] / len(X_batch))
                 v_b[j] = beta2 * v_b[j] + (1 - beta2) * ((db[j] / len(X_batch))**2)
 
-                m_w_cap = m_w[j] / (1 - beta1**(epoch + 1))
-                v_w_cap = v_w[j] / (1 - beta2**(epoch + 1))
-                m_b_cap = m_b[j] / (1 - beta1**(epoch + 1))
-                v_b_cap = v_b[j] / (1 - beta2**(epoch + 1))
+                m_w_cap = m_w[j] / (1 - beta1**t)
+                v_w_cap = v_w[j] / (1 - beta2**t)
+                m_b_cap = m_b[j] / (1 - beta1**t)
+                v_b_cap = v_b[j] / (1 - beta2**t)
 
                 weights[j] -= (learning_rate * m_w_cap) / (np.sqrt(v_w_cap) + epsilon)
                 biases[j] -= (learning_rate * m_b_cap) / (np.sqrt(v_b_cap) + epsilon)
@@ -406,28 +340,13 @@ def minibatch_adam(X, Y, weights, biases, num_layers, activation, learning_rate,
         })
 
         print("epoch:", epoch + 1, "loss:", epoch_loss," & accuracy:", accuracy)
-#########---------
-    # compute confusion matrix
-    Y_pred_labels = np.argmax(Y_val_cap, axis = 1)
-    Y_true_labels = np.argmax(Y_val, axis = 1)
-    conf_matrix = confusion_matrix(Y_true_labels, Y_pred_labels)
-
-    # plot confusion matrix
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(conf_matrix, annot = True, fmt = "d", cmap = "Blues")
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
-    plt.title("Confusion Matrix")
-
-    #  log confusion matrix to wandb
-    wandb.log({"confusion_matrix": wandb.Image(plt)})
-######-------------------
-    #return losses
+    return weights, biases
 
 ################################
 # minibatch nadam  gradient decent
 def minibatch_nadam(X, Y, weights, biases, num_layers, activation, learning_rate, epochs, batch_size, beta1, beta2, epsilon, loss_fun, weight_decay):
     samples_size = X.shape[0]
+    t = 0 # step
 
     # shuffle dataset before splitting
     indices = np.random.permutation(samples_size)
@@ -457,24 +376,25 @@ def minibatch_nadam(X, Y, weights, biases, num_layers, activation, learning_rate
 
             for x, y in zip(X_batch, Y_batch):
                 grads_W, grads_b = compute_grads(x, y, num_layers, weights, biases, activation, weight_decay)
-                for j in range(len(weights)):
-                    dw[j] = grads_W[j]
-                    db[j] = grads_b[j]
 
+                dw = grads_W
+                db = grads_b
+
+            t += 1 # increment step
             for j in range(len(weights)):
                 m_w[j] = beta1 * m_w[j] + (1 - beta1) * (dw[j] / len(X_batch))
                 v_w[j] = beta2 * v_w[j] + (1 - beta2) * ((dw[j] / len(X_batch))**2)
                 m_b[j] = beta1 * m_b[j] + (1 - beta1) * (db[j] / len(X_batch))
                 v_b[j] = beta2 * v_b[j] + (1 - beta2) * ((db[j] / len(X_batch))**2)
 
-                m_w_cap = m_w[j] / (1 - beta1**(epoch + 1))
-                v_w_cap = v_w[j] / (1 - beta2**(epoch + 1))
-                m_b_cap = m_b[j] / (1 - beta1**(epoch + 1))
-                v_b_cap = v_b[j] / (1 - beta2**(epoch + 1))
+                m_w_cap = m_w[j] / (1 - beta1**t)
+                v_w_cap = v_w[j] / (1 - beta2**t)
+                m_b_cap = m_b[j] / (1 - beta1**t)
+                v_b_cap = v_b[j] / (1 - beta2**t)
 
                 # nesterov momentum in nadam
-                m_w_lookahead = beta1 * m_w_cap + (1 - beta1) * (dw[j] / len(X_batch)) / (1 - beta1**(epoch + 1))
-                m_b_lookahead = beta1 * m_b_cap + (1 - beta1) * (db[j] / len(X_batch)) / (1 - beta1**(epoch + 1))
+                m_w_lookahead = beta1 * m_w_cap + (1 - beta1) * (dw[j] / len(X_batch)) / (1 - beta1**t)
+                m_b_lookahead = beta1 * m_b_cap + (1 - beta1) * (db[j] / len(X_batch)) / (1 - beta1**t)
 
                 weights[j] -= (learning_rate * m_w_lookahead) / (np.sqrt(v_w_cap) + epsilon)
                 biases[j] -= (learning_rate * m_b_lookahead) / (np.sqrt(v_b_cap) + epsilon)
@@ -500,20 +420,4 @@ def minibatch_nadam(X, Y, weights, biases, num_layers, activation, learning_rate
         })
 
         print("epoch:", epoch + 1, "loss:", epoch_loss," & accuracy:", accuracy)
-#########---------
-    # compute confusion matrix
-    Y_pred_labels = np.argmax(Y_val_cap, axis = 1)
-    Y_true_labels = np.argmax(Y_val, axis = 1)
-    conf_matrix = confusion_matrix(Y_true_labels, Y_pred_labels)
-
-    # plot confusion matrix
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(conf_matrix, annot = True, fmt = "d", cmap = "Blues")
-    plt.xlabel("Predicted")
-    plt.ylabel("Actual")
-    plt.title("Confusion Matrix")
-
-    #  log confusion matrix to wandb
-    wandb.log({"confusion_matrix": wandb.Image(plt)})
-######-------------------
-    #return losses
+    return weights, biases
